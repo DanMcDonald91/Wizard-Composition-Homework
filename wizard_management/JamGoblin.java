b22c0fe1cacd43bdb1f63d0537561a0ad738adca
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-02-15 21:35:29 +0000

    created test and function for the name of jamgoblin

---
+++ wizard_management/JamGoblin.java
@@ -0,0 +1,8 @@
+package wizard_management;
+
+public class JamGoblin extends Villain {
+  public JamGoblin(String name){
+    super(name);
+  }
+}
+
---
+++ wizard_management_specs/JamGoblinTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import wizard_management.*;
+
+public class JamGoblinTest {
+
+  JamGoblin jamgoblin;
+
+  @Before
+  public void before(){
+    jamgoblin = new JamGoblin("The Red King");
+  }
+
+  @Test
+  public void hasName(){
+    assertEquals("The Red King", jamgoblin.getName());
+  }
+}
